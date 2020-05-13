52f9df21f25c94c32eb85f476d6c4b4a297cc155
Ramon Audain <raudain123@gmail.com>
2020-05-13 09:13:01 -0700

    uncomment

--- main.js
+++ main.js
@@ -2,36 +2,36 @@
 function palindrome(str) {
   // Palindromes are the same forwards or backwards
 
-  // // Solution #1
-  // let reversed = ""
+  // Solution #1
+  let reversed = "";
 
-  // for(character of str) {
-  //   reversed = character + reversed;
-  // }
+  for(let character of str) {
+    reversed = character + reversed;
+  }
 
-  // return str === reversed ? true : false;
-
-
-  // // Solution #2
-  // const reversed = str.split("").reverse().join("");
-
-  // if (str === reversed){
-  //   return true;
-  // }
-  // else{
-  //   return false;
-  // }
+  return str === reversed ? true : false;
 
 
-  // // Solution #3
-  // let reversed = str.split("").reduce((reverse, character) => character + reverse, "");
+  // Solution #2
+  const reversed = str.split("").reverse().join("");
+
+  if (str === reversed){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+
+  // Solution #3
+  let reversed = str.split("").reduce((reverse, character) => character + reverse, "");
   
-  // // Returns boolean
-  // return str === reversed;
+  // Returns boolean
+  return str === reversed;
 
 
   // Solution #4
-  return str.split("").every((character, index) => character === str[str.length - index - 1])
+  return str.split("").every((character, index) => character === str[str.length - index - 1]);
 
 }
 
